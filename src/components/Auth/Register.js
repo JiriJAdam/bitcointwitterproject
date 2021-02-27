5d8c6fedd37a6c0d21bf59fcab4883d297085d0a
Jiri Adam <jiriadam@Jiris-MacBook-Pro.local>
2021-02-26 23:25:30 -0600

    register page

--- src/components/Auth/Register.js
+++ src/components/Auth/Register.js
@@ -25,35 +25,63 @@ class Register extends Component {
             password: '',
             password2: ''
         }
+        this.handleChange = this.handleChange.bind(this)
+        this.handleSubmit = this.handleSubmit.bind(this)
+    }
+    handleChange (e) {
+        this.setState({ [e.target.name]: e.target.value })
+    }
+    handleSubmit (e) {
+        e.preventDefault()
+        const userData = {
+            email: this.state.email,
+            login: this.state.login,
+            password: this.state.password,
+            password2: this.state.password2
+        }
+
+        console.log(userData)
     }
     render() {
         const { classes } = this.props;
 
         return (
             <Paper style={{padding: 15}}>
-                <form>
+                <form onSubmit={this.handleSubmit}>
                     <TextField
                         type="email"
                         label="Email"
                         className={classes.textField}
+                        value={this.state.email}
+                        onChange={this.handleChange}
+                        name="email"
                     />
                     <TextField 
                         label="Login"
                         type="text"
+                        name="login"
+                        value={this.state.login}
+                        onChange={this.handleChange}
                         className={classes.textField}
                     />
                     <TextField 
                         label="Password"
                         type="password"
+                        name="password"
+                        value={this.state.password}
+                        onChange={this.handleChange}
                         className={classes.textField}
                     />
                     <TextField 
                         label="Repeat Password"
                         type="password"
+                        name="password2"
+                        value={this.state.password2}
+                        onChange={this.handleChange}
                         className={classes.textField}
                     />
                     <div className={classes.btnBlock}>
-                        <button variant="outlined">
+                        <button variant="outlined" type="submit">
                             Submit
                         </button>
                     </div>
